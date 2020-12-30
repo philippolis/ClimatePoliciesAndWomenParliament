f3ad5dc9d4d6e6b1f66b0cabf803f4209c79a2a3
philippolis <holz.philipp@gmx.de>
2020-12-30 08:41:17 +0100

    Linear model showed no significance between Share of women in parliament and climate policy performance

--- Script.R
+++ Script.R
@@ -1,14 +1,13 @@
 library(ggplot2)
 library(dplyr)
 
-
 # Data wrangling Women in Parliament ----
 WomenParliament_data <- read.csv("WomenParliament.csv", sep = ";")
 
 ## Creating a new column with overall share of women
 ### Replacing NA with 0 in lower house columns
 WomenParliament_data[is.na(WomenParliament_data)] <- 0
-                                                                                                      WomenParliament_data$LowerOrSingleHouse_Seats)
+
 WomenParliament_data$Share <- (WomenParliament_data$UpperOrSenate_Women + WomenParliament_data$LowerOrSingleHouse_Women)/
   (WomenParliament_data$UpperOrSenate_Seats + WomenParliament_data$LowerOrSingleHouse_Seats)
 
@@ -27,7 +26,6 @@ setdiff(countries_Climate, countries_Women) # shows which states are in Climate 
 
 WomenParliament_data$Country <- gsub("Russian Federation", "Russia", WomenParliament_data$Country)
 WomenParliament_data$Country <- gsub("United States of America", "United States", WomenParliament_data$Country)
-WomenParliament_data$Country <- gsub("Iran (Islamic Republic of)", "Iran", WomenParliament_data$Country) # didn't work
 WomenParliament_data$Country <- gsub("Republic of Korea", "South Korea", WomenParliament_data$Country)
 
 WomenParliament_data_small <- WomenParliament_data_small[WomenParliament_data_small$Country %in% countries_Climate,]
@@ -42,11 +40,13 @@ names(ClimatePolicyIndex_data)[names(ClimatePolicyIndex_data)=="value"] <- "Inde
 ## Only selecting variables of interest
 ClimatePolicyIndex_data_small <- ClimatePolicyIndex_data %>% select(Country, Index)
 
-
-# Joining ClimatePolicy and WomenParliament data
+# Joining ClimatePolicy and WomenParliament data ----
 data = WomenParliament_data_small %>% 
   full_join(ClimatePolicyIndex_data_small, by = c("Country"))
 
+## Making a test plot
 ggplot(data, aes(x = Share, y = Index)) + geom_point() + geom_smooth(method = "lm")
 
+## Checking if linear model is significant
+summary(lm(data = data, Index ~ Share))
 
