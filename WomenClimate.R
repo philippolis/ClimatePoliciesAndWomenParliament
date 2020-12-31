8fbe536a3a6fc39295916178e605b1ec5ec1c85e
philippolis <holz.philipp@gmx.de>
2020-12-31 16:07:35 +0100

    Cared for two more independend variables. Beginning of Bayesian analysis

--- WomenClimate.R
+++ WomenClimate.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(countrycode)
 library(tidyr)
 library(tabulizer)
+library(rstanarm)
 
 # Women in Parliament ----
 ## Loading data
@@ -31,12 +32,6 @@ WomenParliament_2020 <- WomenParliament_2020 %>% select(Country, Year, Total_Per
 ## Joining the Datasets
 WomenParliament <- rbind(WomenParliament_2018, WomenParliament_2019, WomenParliament_2020)
 
-## Cleaning up the environment
-rm(WomenParliament_2018)
-rm(WomenParliament_2019)
-rm(WomenParliament_2020)
-rm(i)
-
 # Policy Performance ----
 pdf_link_2018 <- "https://germanwatch.org/sites/germanwatch.org/files/publication/20503.pdf"
 pdf_link_2019 <- "https://germanwatch.org/sites/germanwatch.org/files/CCPI-2019-Results-190614-WEB-A4.pdf"
@@ -53,7 +48,7 @@ pdf_data_2018 <- pdf_data_2018 %>%
   mutate(Rank = as.numeric(Rank))
 
 for (i in 1:nrow(pdf_data_2018)) {
-  pdf_data_2018[i, "Score"] <- round(100 - i/length(pdf_data_2018$Score) * 100, digits = 1)
+  pdf_data_2018[i, "Score"] <- round(100 - i/length(pdf_data_2018$Rank) * 100, digits = 1)
 }
 
 ### Scraping Index 2019
@@ -61,16 +56,10 @@ pdf_data_2019 <- as.data.frame(extract_areas(pdf_link_2019, pages = 15, method =
 
 colnames(pdf_data_2019) <- c("Rank", "Country", "Score")
 
-pdf_data_2019 <- pdf_data_2019 %>% 
-  mutate(Rank = as.numeric(Rank))
-
 ### Scraping Index 2020
 pdf_data_2020 <- as.data.frame(extract_areas(pdf_link_2020, pages = 17, method = "stream"))
 
 colnames(pdf_data_2020) <- c("Rank", "Country", "Score")
-
-pdf_data_2020 <- pdf_data_2020 %>% 
-  mutate(Rank = as.numeric(Rank))
 
 ## Equalizing Country columns
 for(i in 1:nrow(pdf_data_2018)) {
@@ -114,11 +103,46 @@ data <- data %>%
 data <- data %>% 
   mutate(Total_PercWomen = as.numeric(Total_PercWomen))
 
+## Adding a column with region
+for(i in 1:nrow(data)) {
+  data[i,"Region"] <- countryname(data[i,"Country"], destination = "region", warn = TRUE)
+}
+
+data <- data[complete.cases(data$Country),]
+
+## Summarizing the regions
+data$Region <- gsub("\\<East Asia & Pacific\\>", "South Asia, East Asia & Pacific", data$Region)
+data$Region <- gsub("\\<South Asia\\>", "South Asia, East Asia & Pacific", data$Region)
+data$Region <- gsub("\\<Latin America & Caribbean\\>", "Americas", data$Region)
+data$Region <- gsub("\\<Middle East & North Africa\\>", "Middle East & Africa", data$Region)
+data$Region <- gsub("\\<Sub-Saharan Africa\\>", "Middle East & Africa", data$Region)
+data$Region <- gsub("\\<North America\\>", "Americas", data$Region)
+data$Region <- gsub("\\<South Asia, East Asia & Pacific, East Asia & Pacific\\>", "South Asia, East Asia & Pacific", data$Region)
+
 # Visualizing the relationship
-ggplot(data = data, aes(x = Total_PercWomen, y = Score)) +
-  geom_point() +
-  geom_smooth(method = "lm") # hoher Zusammenhang
+ggplot(data = data) +
+  geom_point(aes(x = Total_PercWomen, y = Score, color = Region)) +
+  geom_smooth(aes(x = Total_PercWomen, y = Score), method = "lm") +
+  xlab("Share of Women in Parliament") +
+  ylab("Climate Policy Index")
 
 summary(lm(data = data, Score ~ Total_PercWomen)) # Statistische Signifikanz
 
-        
+ggplot(data = data) +
+  geom_point(aes(x = Total_PercWomen, y = Score)) +
+  geom_smooth(aes(x = Total_PercWomen, y = Score, color = factor(Year)), method = "lm", se = F) +
+  facet_wrap(~ Region)
+
+ggplot(data = data) +
+  geom_point(aes(x = Total_PercWomen, y = Score, color = Region)) +
+  geom_smooth(aes(x = Total_PercWomen, y = Score), method = "lm") +
+  facet_wrap(~ factor(Year))
+
+lm_model <- lm(data = data, Score ~ Total_PercWomen + factor(Year) + Region)
+
+summary(lm_model) # Statistische Signifikanz
+
+# Bayesian Regression analysis
+stan_model <- stan_glm(data = data, Score ~ Total_PercWomen)
+
+summary(stan_model) # Rhat below 1.1, so model converged
