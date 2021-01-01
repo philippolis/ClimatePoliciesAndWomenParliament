2dc317ee37678a7ef8cf94f06350c3ef18adc0e2
philippolis <holz.philipp@gmx.de>
2021-01-01 13:10:35 +0100

    Added first plot and created first pdf

--- WomenClimate_DataAnalysis.R
+++ WomenClimate_DataAnalysis.R
@@ -1,6 +1,23 @@
 library(rstanarm)
+library(extrafont)
+loadfonts()
 
-data <- read.csv("data.csv")
+data <- read.csv("WomenClimate_Data.csv")
+
+# Bivariate plot
+
+ggplot(data = data) +
+  geom_point(aes(x = PercentWomen, y = PolicyScore), alpha = 0.5) +
+  geom_smooth(aes(x = PercentWomen, y = PolicyScore), method = "lm") +
+  xlab("Share of Women in Parliament") +
+  ylab("Climate Policy Index") +
+  scale_x_continuous(labels = scales::percent_format(accuracy = 1), 
+                     breaks = scales::pretty_breaks(n = 6)) +
+  ggtitle("Share of Women in Parliament and Climate Policy Index") +
+  theme(
+     plot.title = element_text(size = 11, face = "bold")
+  )
+
 
 # Bayesian Regression analysis
 stan_model <- stan_glm(data = data, Score ~ Total_PercWomen)
